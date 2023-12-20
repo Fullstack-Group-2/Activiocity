a37bef17855ca383b28c083f73281d1957692f96
Ahmad Ali Tresvant <ahmadtresvant@Alis-M1-Pro.local>
2023-12-20 15:37:18 -0500

    added single activity component that is designed to fetch a activity and dsplay its columns

---
+++ src/client/components/singleActvities.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+
+const singleActivities = () => {
+  const { id } = useParams();
+  const [activity, setActivity] = useState({});
+  console.log("activity", activity);
+
+  useEffect(() => {
+    const getActivity = async () => {
+      try {
+        const { data: foundActivity } = await axios.get(
+          `/api/activities/${id}`
+        );
+        setActivity(foundActivity);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getActivity();
+  }, []);
+
+  return (
+    <div>
+      <div>
+        <h3>{activity.title}</h3>
+        <h3>{activity.description}</h3>
+        <h3>{activity.location}</h3>
+        <h3>{activity.seasonCategory}</h3>
+        <h3>{activity.isWet ? "Yes" : "No"}</h3>
+        <h3>{activity.rating}</h3>
+        <h3>{activity.imageURL}</h3>
+
+      </div>
+    </div>
+  );
+};
+
+
+
+export default singleActivities;
