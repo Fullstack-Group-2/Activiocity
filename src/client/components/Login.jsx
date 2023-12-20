567c4a7bd191aa71239da9a0304bdd0dadd37a33
michaelwhendrix <michael.w.hendrix@gmail.com>
2023-12-20 06:51:04 -0600

    Added login form on Login page in Client side. Functioning to a console log

--- src/client/components/Login.jsx
+++ src/client/components/Login.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = () => {
+        console.log("username: " ,username, " password: ", password);
+    }
     return (
         <>
             <h1>Login</h1>
+            <div>
+                <input
+                    placeholder="username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}/>
+                <input
+                    placeholder="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}/>
+                <button onClick={handleLogin}>Login</button>
+            </div>
         </>
     );
 }
