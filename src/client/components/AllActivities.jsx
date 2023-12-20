ea509c9de113b2dacbae3e5bee4c76807bd5baf0
Ahmad Ali Tresvant <ahmadtresvant@Alis-M1-Pro.local>
2023-12-20 14:26:33 -0500

    still need api/activities route to complete this branch but everthing else seems to be in the right order

--- src/client/components/AllActivities.jsx
+++ src/client/components/AllActivities.jsx
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import {Link} from "react-router-dom";
 
 const AllActivities = () => {
-    return (
-        <>
-            <h1>All Activities</h1>
-        </>
-    );
-}
+  const [activities, setActivities] = useState([]);
+  const [search, setSearch] = useState("")
+
+  const filtered = activities.filter(
+    (activity) =>
+      activity.name.includes(search)
+  );
+
+  useEffect(() => {
+    const getActivities = async () => {
+      try {
+        const { data: foundActivities } = await axios.get("/api/activities");
+        setActivities(foundActivities);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getActivities();
+  }, []);
+
+  return (
+    <>
+      <h1>All Activities</h1>
+      <div>
+        <input
+          placeholder="search..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <button onClick={() => setSearch("")}>X</button>
+        {search.length == 0
+          ? activities.map((activities) => (
+              <div key={activities.id} style={{ border: "2px solid black" }}>
+                <Link to={`/${activities.id}`}>
+                  <h3>Name: {activities.name}</h3>
+                </Link>
+                <h3>Description: {activities.description}</h3>
+              </div>
+            ))
+          : filtered.map((activities) => (
+              <div key={activities.id} style={{ border: "2px solid black" }}>
+                <Link to={`/${activities.id}`}>
+                  <h3>Name: {activities.name}</h3>
+                </Link>
+                <h3>Description: {activities.description}</h3>
+              </div>
+            ))}
+      </div>
+    </>
+  );
+};
 export default AllActivities;
