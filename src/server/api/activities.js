8b172671411a997fb32bd9e63bf2bb1638c630df
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-12-20 11:47:21 -0600

    created util and client in server folder so that activity js will run with prisma and verify. set the requires and routes to start activity CRUD with get all and get specific activity

--- src/server/api/activities.js
+++ src/server/api/activities.js
@@ -1,4 +1,27 @@
 const express = require("express");
 const router = express.Router();
+const prisma = require("../client");
+const { verify } = require("../utils");
+
+//get all activities from database
+router.get("/", async (req, res, next) => {
+  try {
+    const activities = await prisma.activity.findMany();
+
+    res.send(activity);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+//get activities from database by id
+router.get("/:id", async (req, res) => {
+  const activityId = parseInt(req.params.id);
+
+  const activity = await prisma.activity.findUnique({
+    where: { id: activityId },
+  });
+  res.send(activity || {});
+});
 
 module.exports = router;
