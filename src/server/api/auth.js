590e4db4fc9210625574b4a8a9914367fbd4830f
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-12-20 22:50:01 -0600

    trying to find bug in code

--- src/server/api/auth.js
+++ src/server/api/auth.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const prisma = require("../client");
+const saltRounds = 10;
 
 router.post("/register", async (req, res, next) => {
   const { username, password } = req.body;
   const saltRounds = 10;
   const hashedPassword = await bcrypt.hash(password, saltRounds);
+  try{
+    const user = await.prisma.user.create({
+      data: {
+        username,
+        password: hashedPassword;
+      }
+    });
+    const token=jwt.sign(
+      {id: user.id, username: user.username};
+      process.env.JWT_SECRET
+    );
+  }
 });
