2a6d67cb16684d3ffd9a9e2e867bdc6c9774b273
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-12-20 22:46:49 -0600

    working on route function

--- src/server/api/auth.js
+++ src/server/api/auth.js
@@ -4,4 +4,6 @@ const prisma = require("../client");
 
 router.post("/register", async (req, res, next) => {
   const { username, password } = req.body;
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
 });
