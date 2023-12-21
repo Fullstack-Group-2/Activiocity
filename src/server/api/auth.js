a2dd54f3649ca97493821d30ceb221a81f14c722
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-12-21 10:37:52 -0600

    created a login route in auth.js

--- src/server/api/auth.js
+++ src/server/api/auth.js
@@ -25,4 +25,41 @@ router.post("/register", async (req, res, next) => {
     console.error(err);
   }
 });
+
+router.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res
+      .status(418)
+      .send({
+        message:
+          "Access DENIED!!!!: Incorrect USERNAME or PASSWORD: Not one of the kool kidz!",
+      });
+    return;
+  }
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username,
+      },
+    });
+
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+      res
+        .status(401)
+        .send({ message: "Nice try Goldilocks! but not today JR!" });
+      return;
+    }
+    const token = jwt.sign(
+      { id: user.id, username: user.username },
+      process.env.JWT_SECRET
+    );
+    res.status(200).send({ token });
+  } catch (err) {
+    console.err(err);
+  }
+});
+
 module.exports = router;
