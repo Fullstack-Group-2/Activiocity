da486baf37cdfe7ede5fe74e631276ac6b2c062e
Steven D. Kaste <steven.d.kaste@Stevens-MacBook-Pro.local>
2023-12-20 22:53:10 -0600

    added the required framework

--- src/server/api/auth.js
+++ src/server/api/auth.js
@@ -1,24 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const prisma = require("../client");
-const saltRounds = 10;
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 router.post("/register", async (req, res, next) => {
   const { username, password } = req.body;
   const saltRounds = 10;
   const hashedPassword = await bcrypt.hash(password, saltRounds);
-  try{
-    const user = await.prisma.user.create({
+  try {
+    const user = await prisma.user.create({
       data: {
         username,
         password: hashedPassword,
-      }
+      },
     });
-    const token=jwt.sign(
-      {id: user.id, username: user.username},
+    const token = jwt.sign(
+      { id: user.id, username: user.username },
       process.env.JWT_SECRET
     );
-  }catch(err){
+  } catch (err) {
     console.error(err);
   }
 });
