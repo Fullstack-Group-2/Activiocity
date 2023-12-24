f4b59cce015e886e1ec61b75770aa4fba7b06509
King Gambit Lawrence <kinggambitlawrence@kings-air.mynetworksettings.com>
2023-12-24 16:14:10 -0500

    updated user CRUD not done

--- src/server/auth/user.cjs
+++ src/server/auth/user.cjs
@@ -1,3 +1,68 @@
-const router = require("express").Router();
+const express = require("express");
+const router = express.Router();
+const prisma = require("../client.cjs");
+
+// all users.....  /auth/user
+router.get("/", async (req, res, next) => {
+    try {
+      const users = await prisma.user.findMany();
+  
+      res.send(users);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+//single user by id........ /auth/user/number of id
+  router.get("/:id", async (req, res) => {
+    const userId = parseInt(req.params.id);
+  
+    const singleUser = await prisma.user.findUnique({
+      where: { id: userId },
+    });
+    res.send(singleUser || {});
+  });
+
+  // create a user
+  router.post("/register", async (req, res) => {
+    const newUser = req.body;
+  
+    if (!req.user) {
+      res.sendStatus(401);
+    } else {
+      try {
+        const result = await prisma.user.create({
+          data: {
+                username:"",
+                password:""
+            }
+        });
+        res.send(result);
+      } catch (err) {
+        res.send(err);
+      }
+    }
+  });
+
+  // update an user
+  router.patch("/", async (req, res) => {
+    const editUser = req.body;
+  
+    if (!req.user) {
+      res.sendStatus(401);
+    } else {
+      try {
+        const updatedresult = await prisma.user.patch({
+          data: editUser,
+        });
+        res.send(updatedresult);
+      } catch (err) {
+        res.send(err);
+      }
+    }
+  });
 
 module.exports = router;
+
+
+
