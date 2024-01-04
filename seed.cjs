3311c5c403c6d54a04bc3d619920ecb61800a96a
King Gambit Lawrence <kinggambitlawrence@kings-air.mynetworksettings.com>
2024-01-04 14:32:01 -0500

    added activities in seed file

--- seed.cjs
+++ seed.cjs
@@ -26,8 +26,8 @@ async function seed() {
       password: hashedSteven,
       isAdmin: true,
     },
-  });
-
+  }); 
+  
   const hashedMichael = await bcrypt.hash("password", saltRounds);
   const Michael = await prisma.user.create({
     data: {
@@ -89,12 +89,97 @@ async function seed() {
     },
   });
 
+  const bowling = await prisma.activity.create({
+    data: {
+      title: "Pin Tower",
+      description: "Bowling great for family gatherings",
+      location: "Atlanta",
+      seasonCategory: "Fall",
+      isWet: false,
+      rating: 3,
+      imageURL:
+        "https://www.jbcharleston.com/images/fun/bowling-centers/hero/marringtonbowling.jpg",
+    },
+  });
+
+  const castino = await prisma.activity.create({
+    data: {
+      title: "Best of 21",
+      description: "Test your luck",
+      location: "Las Vegas",
+      seasonCategory: "Winter",
+      isWet: false,
+      rating: 4,
+      imageURL:
+        "https://a.cdn-hotels.com/gdcs/production153/d924/dd73480c-6248-4372-ac5c-a17c7bf9069f.jpg",
+
+    },
+  });
+
+  const archery = await prisma.activity.create({
+    data: {
+      title: "Shoot like Robin Hood",
+      description: "Perfect your aim with archery",
+      location: "New Jersey",
+      seasonCategory: "Summer",
+      isWet: false,
+      rating: 2,
+      imageURL:
+        "https://www.goodnet.org/photos/620x0/39300_hd.jpg",
+
+    },
+  });
+
+  const paraSailing = await prisma.activity.create({
+    data: {
+      title: "Fly like a Bird",
+      description: "View the city from a different angle",
+      location: "Jamaica",
+      seasonCategory: "Spring",
+      isWet: true,
+      rating: 5,
+      imageURL:
+        "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/1b/58/f5/cd/island-parasail.jpg?w=1200&h=-1&s=1",
+
+    },
+  });
+
+  const scubaDiving  = await prisma.activity.create({
+    data: {
+      title: "Swim like a fish",
+      description: "View the beautiful world under the sea",
+      location: "Atlantic Ocean",
+      seasonCategory: "Summer",
+      isWet: true,
+      rating: 4,
+      imageURL:
+        "https://www.keywestscubadiving.com/wp-content/uploads/sites/62/sites/8/2019/07/Key-West-Scuba-Diving-3-2048x1365.jpg",
+
+    },
+  });
+
+  const hiking = await prisma.activity.create({
+    data: {
+      title: "Walk til you drop",
+      description: "Beautiful walk in the outdoors",
+      location: "Tropics",
+      seasonCategory: "Summer",
+      isWet: true,
+      rating: 5,
+      imageURL:
+        "https://images.downeast.com/wp-content/uploads/2023/06/Waterfalls-fb-jpg.webp",
+
+    },
+  });
+
+  
+
   const review1 = await prisma.review.create({
     data: {
       userId: Ahmad.id,
       activityId: camping.id,
       rating: 2,
-      review: "lskdfjsdl",
+      review: "Camping was amazing but I hated the bugs",
     },
   });
 
@@ -103,18 +188,38 @@ async function seed() {
       userId: Steven.id,
       activityId: snowboarding.id,
       rating: 5,
-      review: "laskdjfldsakjf",
+      review: "Best thing I ever did",
     },
   });
 
   const review3 = await prisma.review.create({
     data: {
       userId: Michael.id,
-      activityId: skiing.id,
+      activityId: scubaDiving.id,
       rating: 3,
-      review: "sldkfjsd",
+      review: "It was nice",
     },
   });
+
+
+  const review4 = await prisma.review.create({
+    data: {
+      userId: Michael.id,
+      activityId: castino.id,
+      rating: 1,
+      review: "I lost all my money, drinks were great tho :(",
+    },
+  });
+
+
+  // const review5 = await prisma.review.create({
+  //   data: {
+  //     userId: ,
+  //     activityId: scubaDiving.id,
+  //     rating: 3,
+  //     review: "It was nice",
+  //   },
+  // });
 
   const comment1 = await prisma.comment.create({
     data: {
