7ce051b5a4cfd5685e6aa23bf1c23df0e44f377f
Ahmad Ali Tresvant <ahmadtresvant@Alis-M1-Pro.local>
2024-01-04 13:55:43 -0500

    reviews will show on the singleActivities page

--- src/client/components/singleActvities.jsx
+++ src/client/components/singleActvities.jsx
@@ -6,20 +6,30 @@ import CreateReview from "./createReview";
 const SingleActivities = () => {
   const { id } = useParams();
   const [activity, setActivity] = useState({});
-  console.log("activity", activity);
+  const [reviews, setReviews] = useState([]); // State for storing reviews
 
   useEffect(() => {
     const getActivity = async () => {
       try {
-        const { data: foundActivity } = await axios.get(
-          `/api/activities/${id}`
-        );
+        const { data: foundActivity } = await axios.get(`/api/activities/${id}`);
         setActivity(foundActivity);
       } catch (error) {
         console.error(error);
       }
     };
     getActivity();
+  }, [id]);
+
+  useEffect(() => {
+    const getReviews = async () => {
+      try {
+        const { data: foundReviews } = await axios.get("/api/reviews");
+        setReviews(foundReviews);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getReviews();
   }, []);
 
   return (
@@ -29,20 +39,28 @@ const SingleActivities = () => {
         <h3>{activity.description}</h3>
         <h3>The desired activity location is in {activity.location}</h3>
         <h3>The best time to do this activity is {activity.seasonCategory}</h3>
-        <h3>
-          Is it possible to get wet doing this? {activity.isWet ? "Yes" : "No"}
-        </h3>
-        <h3>
-          This activity has a popularity rating of {activity.rating} out of 5
-        </h3>
+        <h3>Is it possible to get wet doing this? {activity.isWet ? "Yes" : "No"}</h3>
+        <h3>This activity has a popularity rating of {activity.rating} out of 5</h3>
         <img src={activity.imageURL} width="640" height="426" />
       </div>
 
       <div>
-        <CreateReview id = {id} />
+        <h2>Reviews</h2>
+        {reviews.map((review) => (
+          <div key={review.id}>
+            <p>Review: {review.review}</p>
+            <p>Rating: {review.rating}</p>
+            {/* Other review details can be displayed here */}
+          </div>
+        ))}
+      </div>
+
+      <div>
+        <CreateReview id={id} />
       </div>
     </div>
   );
 };
 
 export default SingleActivities;
+
